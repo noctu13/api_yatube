98338510f4886db0b7d0d0b9cc359e0f55ded293
Sapraliev Mikhail <13bit57@gmail.com>
2020-05-23 13:13:49 +0300

    release 0.2

--- posts/views.py
+++ posts/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import get_object_or_404
+
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-
+from .permissions import IsOwnerOrReadOnly
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         return serializer.save(author=self.request.user)
 
 
 class CommentViewSet(ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         queryset = Comment.objects.filter(post=post)
         return queryset
-
+    
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         return serializer.save(author=self.request.user, post=post)
